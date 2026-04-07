2c9d522c3e325f3cf68408fd5ea43d41938a2cbb
Felipe Vitor Espada <felipevitorespada23@gmail.com>
2026-04-07 16:35:44 -0300

    chore: create a simple system using method post

---
+++ php/exercicio-post/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teste</title>
+</head>
+<body>
+    <h1>Formulário</h1>
+    <form action="recebe.php" method="post">
+        <input type="text" name="nome" placeholder="Nome">
+        <input type="text" name="idade" placeholder="Idade">
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
